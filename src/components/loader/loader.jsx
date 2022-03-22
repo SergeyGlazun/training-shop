36e4ea79de380140e121cd9ca01fb13166d925a2
Vasiliy Pupkin <vpupkin@company.com>
2022-03-22 16:31:55 +0300

    apdateSpiner

--- src/components/loader/loader.jsx
+++ src/components/loader/loader.jsx
@@ -1,4 +1,7 @@
 import './loader.scss';
 export const Loader = () => (
-<div className="lds-ellipsis" data-test-id='loader' ><div></div><div></div><div></div><div></div></div>
+    <div className='blockSpiner'>
+        <div className="lds-ellipsis" data-test-id='loader' ><div></div><div></div><div></div><div></div></div>
+    </div>
+
 )
