50208ba9d76d5d001cc23d09bc83f11e4516b523
Vasiliy Pupkin <vpupkin@company.com>
2022-04-04 14:02:22 +0300

    apdateCod

--- src/reducers/saga/emailSaga.js
+++ src/reducers/saga/emailSaga.js
@@ -1,44 +1,3 @@
-// import {
-//     all,
-//     put,
-//     call,
-//     takeLatest
-// } from 'redux-saga/effects';
-// import axios from 'axios';
-// import {
-//     userEmailAction,
-//     responseAction,
-//     loadingAction,
-//     validationChek
-// } from '../actionEmailPost';
-
-
-// function* emailSagaPost(action) {
-//     console.log(action.payload);
-//     yield put(loadingAction(true));
-//     try {
-//         const responce = yield call(axios.post, "https://training.cleverland.by/shop/email", action);     
-//         yield put(responseAction(responce.statusText));
-      
-//         yield put(validationChek(""));
-//     } catch (err) {
-      
-//         yield put(validationChek(action.payload.email));
-//          put(responseAction(err.message));
-      
-//         console.log(err.message);
-//     }
-//     yield put(loadingAction(false));
-// }
-
-// function* emailSagaPostWatcher() {
-//     yield all([takeLatest(userEmailAction().type, emailSagaPost)]);
-// }
-
-// export default emailSagaPostWatcher;
-//////////////////////////////////////////////////
-
-
 import {
     all,
     put,
@@ -59,7 +18,6 @@ import {
 
 function* emailSagaPost(action) {
    
-  console.log(action.payload);
 yield put(loadingAction(true));
     try {
       
